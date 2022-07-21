12f482e48efea575a2a5e5b1680a4070e0726ae5
Sreyas S <76519139+sreyasqb@users.noreply.github.com>
2022-07-21 14:06:39 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -3,33 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ws2;
+package javaapplication1;
 
-/**
- *
- * @author 20pt33
- */
 public class Account {
-    double initBal;
+    double bal;
     String owner;
     String accNo;
     Account(double initBal,String owner,String number){
-        this.initBal=initBal;
+        this.bal=initBal;
         this.owner=owner;
         this.accNo=number;
     }
     Account(double initBal,String owner){
-        this.initBal=initBal;
+        this.bal=initBal;
         this.owner=owner;
-        accNo=String.valueOf((int) Math.random()*1000);   
+        
+        accNo=String.valueOf((int) Math.round(Math.random()*1000));   
     }
     Account(String owner){
-        initBal=0;
-        accNo=String.valueOf((int) Math.random()*1000);
+        bal=0;
+        accNo=String.valueOf((int) Math.round(Math.random()*1000));
         this.owner=owner;
+    }
+    public void deposit(double value){
+        bal+=value;
+    }
+    public void withdraw(double value){
+        if (bal-value>0)
+            bal-=value;
+        else
+            System.out.println("Not enough balance");
+    }
+    public void getDeets(){
+        System.out.println("Owner : "+owner);
+        System.out.println("AccNo : "+accNo);
+        System.out.println("Balance : "+bal);
     }
     public static void main(String args[]){
         Account a=new Account("SUSA");
         System.out.println(a.accNo);
+        a.deposit(100);
+        a.getDeets();
+        a.withdraw(50);
+        a.getDeets();
     }
+    
 }
