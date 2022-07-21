7e05600f666e3d385ca94ac31a2c20d28fcc3cd2
Sreyas S <76519139+sreyasqb@users.noreply.github.com>
2022-07-21 10:49:06 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,5 +1,15 @@
-class Account{
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws2;
 
+/**
+ *
+ * @author 20pt33
+ */
+public class Account {
     double initBal;
     String owner;
     String accNo;
@@ -11,11 +21,15 @@ class Account{
     Account(double initBal,String owner){
         this.initBal=initBal;
         this.owner=owner;
-        
+        accNo=String.valueOf((int) Math.random()*1000);   
     }
     Account(String owner){
         initBal=0;
-        
+        accNo=String.valueOf((int) Math.random()*1000);
+        this.owner=owner;
     }
-
+    public static void main(String args[]){
+        Account a=new Account("SUSA");
+        System.out.println(a.accNo);
+    }
 }
