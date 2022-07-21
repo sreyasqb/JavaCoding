e9eaf5eca06e6323085260626976fc8da31269f3
Sreyas S <76519139+sreyasqb@users.noreply.github.com>
2022-07-21 14:18:41 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -5,25 +5,32 @@
  */
 package javaapplication1;
 
+/**
+ *
+ * @author 20pt33
+ */
 public class Account {
     double bal;
     String owner;
     String accNo;
+    static int numAccounts;
     Account(double initBal,String owner,String number){
         this.bal=initBal;
         this.owner=owner;
         this.accNo=number;
+        numAccounts+=1;
     }
     Account(double initBal,String owner){
         this.bal=initBal;
         this.owner=owner;
-        
+        numAccounts+=1;
         accNo=String.valueOf((int) Math.round(Math.random()*1000));   
     }
     Account(String owner){
         bal=0;
         accNo=String.valueOf((int) Math.round(Math.random()*1000));
         this.owner=owner;
+        numAccounts+=1;
     }
     public void deposit(double value){
         bal+=value;
@@ -39,13 +46,18 @@ public class Account {
         System.out.println("AccNo : "+accNo);
         System.out.println("Balance : "+bal);
     }
+    public static int getNumAccounts(){
+        return numAccounts;
+    }
     public static void main(String args[]){
         Account a=new Account("SUSA");
+        Account b=new Account("BSA");
         System.out.println(a.accNo);
         a.deposit(100);
         a.getDeets();
         a.withdraw(50);
         a.getDeets();
+        System.out.println(Account.getNumAccounts());
     }
     
 }
