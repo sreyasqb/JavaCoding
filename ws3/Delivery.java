1788ce1cfedea51a5600797fcd3258b7bb35f096
Sreyas S <76519139+sreyasqb@users.noreply.github.com>
2022-07-28 15:11:54 +0530

    Create Delivery.java

---
+++ ws3/Delivery.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 20pt35
+ */
+public class Delivery {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        List<Order> orderArr=new ArrayList<Order>();
+//        OrderItem i1=new OrderItem("i12",2,40);
+//        Order o1=new Order();
+//        o1.addOrderedItem(i1);
+//        o1.addOrderedItem(i1);
+//        RushOrder r1=new RushOrder(1);
+//        r1.addOrderedItem(i1);
+//        r1.addOrderedItem(i1);
+//        RushOrder r2=new RushOrder(2);
+//        r2.addOrderedItem(i1);
+//        r2.addOrderedItem(i1);
+//        RushOrder r3=new RushOrder(3);
+//        r3.addOrderedItem(i1);
+//        r3.addOrderedItem(i1);
+//        orderArr.add(o1);
+//        orderArr.add(r1);
+//        orderArr.add(r2);
+//        orderArr.add(r3); 
+//        for (Order o:orderArr){
+//            o.printOrderedItems();
+//        }
+        while (true){
+            Scanner kb=new Scanner(System.in);
+            String input=kb.next();
+            if (input=="done")
+                break;
+            
+        }
+        
+        
+    }
+    
+}
